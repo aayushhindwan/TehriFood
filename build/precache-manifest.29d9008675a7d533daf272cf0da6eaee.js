6789256b720c04e133e42413c93362ecad8dc5fd
Aayush Hindwan <53999933+aayushhindwan@users.noreply.github.com>
2020-09-20 01:11:55 +0530

    Add files via upload

---
+++ build/precache-manifest.29d9008675a7d533daf272cf0da6eaee.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f9f778c2937f5617a5dc6ed5e0bd732",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d519bdf3efbc0fc1ca9a",
+    "url": "/static/css/2.e6f51e71.chunk.css"
+  },
+  {
+    "revision": "f567556622c7e3b0f46d",
+    "url": "/static/css/main.bac2a535.chunk.css"
+  },
+  {
+    "revision": "d519bdf3efbc0fc1ca9a",
+    "url": "/static/js/2.00de3182.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.00de3182.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f567556622c7e3b0f46d",
+    "url": "/static/js/main.076a558b.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "46d9f22baccdf7718ee608f0b5503c6c",
+    "url": "/static/media/landingSVG.46d9f22b.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "24d6c4b34a1d78dc3b94b13a0c5e4a45",
+    "url": "/static/media/pic1.24d6c4b3.jpg"
+  },
+  {
+    "revision": "db90f4828a347909a8985bb9e36f9785",
+    "url": "/static/media/pic2.db90f482.jpg"
+  },
+  {
+    "revision": "db7d19208bc29c3a9f51757dcfb1f3aa",
+    "url": "/static/media/pic3.db7d1920.jpg"
+  },
+  {
+    "revision": "8a8e40517ebdafbc5c599c8ddaaf06ba",
+    "url": "/static/media/pic4.8a8e4051.jpg"
+  }
+]);
